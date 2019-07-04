ebe37cf7f5c2e278690a0955350ae3ec829c7d3d
Seiji Villafranca <seijiralph.villafranca@frontiersoftware.com.ph>
2019-07-04 11:12:37 +0800

    improved ui for menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { Router, NavigationEnd } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-entry-logger';
+  url = '';
+  menuOpen = true;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.url = event.url;
+      }
+    });
+  }
 }
