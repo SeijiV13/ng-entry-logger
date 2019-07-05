f87993627e6489d16450b81b872eb2393aff05d7
Seiji Villafranca <seijiralph.villafranca@frontiersoftware.com.ph>
2019-07-05 08:47:45 +0800

    fixed path change

--- projects/entry-logger/src/lib/components/base/base.component.ts
+++ projects/entry-logger/src/lib/components/base/base.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./base.component.scss']
 })
 export class BaseComponent implements OnInit {
-
+  page = 'home';
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changPage(page) {
+    this.page = page;
   }
 
 }
--- projects/entry-logger/src/lib/components/menu/menu.component.ts
+++ projects/entry-logger/src/lib/components/menu/menu.component.ts
@@ -1,28 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'entry-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent implements OnInit {
-
-  url = '';
+  @Output() pageShift = new EventEmitter();
+  url = 'home';
   menuOpen = true;
 
-  constructor(private router: Router) {
-    router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.url = event.url;
-      }
-    });
-  }
+  constructor() {}
 
   ngOnInit() {
   }
 
   changeRoute(path) {
+    this.url = path;
+    this.pageShift.emit(path);
   }
 
 
